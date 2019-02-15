bc542782ce1720eeab4200c700b6a7d9f81e0a56
wolf <wolf@192.168.10.154>
2019-02-15 10:34:26 +0100

    Change to Java 6

--- src/net/sf/gogui/version/Version.java
+++ src/net/sf/gogui/version/Version.java
@@ -14,7 +14,7 @@ public final class Version
     */
     public static String get()
     {
-        return "1.5preAlpha";
+        return "1.5.0alpha";
     }
 
     /** Make constructor unavailable; class is for namespace only. */
